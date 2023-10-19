f8e27a0cd306da7d5f94d7c748a45e00735f25dc
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-10-19 11:55:01 -0700

    Agregar archivos de proyecto.

---
+++ Portafolio_OAEA/Datos/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+public class Conexion
+{
+    private string cadenaSql = string.Empty;
+
+    public Conexion()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        cadenaSql = builder.GetSection("ConnectionStrings:cadenaSql").Value;
+    }
+
+    public string GetCadenaSql()
+    {
+        return cadenaSql;
+    }
+}
---
+++ Portafolio_OAEA/Models/PortafolioModels.cs
@@ -0,0 +1,17 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Portafolio_OAEA.Models
+{
+    public class PortafolioModels
+    {
+       public int  IdPerfil { get; set; }
+
+       public string Nombre  { get; set; }
+        public string Telefono  { get; set; }
+
+        public string Correo { get; set; }
+  
+        public string Descripcion { get; set; }
+       
+    }
+}
